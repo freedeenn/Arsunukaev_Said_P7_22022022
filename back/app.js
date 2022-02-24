a0069d8f71969b9811d095efb1e5ae4d974a6232
freedeenn <freedeenn@gmail.com>
2022-02-24 01:37:50 +0100

    1

---
+++ back/app.js
@@ -0,0 +1,36 @@
+const { Sequelize, DataTypes } = require("sequelize");
+
+const sequelize = new Sequelize("said", "root", "", {
+	host: "localhost",
+	dialect: "mysql",
+});
+
+async function test() {
+	try {
+		await sequelize.authenticate();
+		console.log("Connection has been established successfully.");
+		await User.sync({ force: true });
+		await User.create({ firstName: "Jane", lastName: "Doe" });
+	} catch (error) {
+		console.error("Unable to connect to the database:", error);
+	}
+}
+test();
+
+const User = sequelize.define(
+	"User",
+	{
+		// Model attributes are defined here
+		firstName: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		lastName: {
+			type: DataTypes.STRING,
+			// allowNull defaults to true
+		},
+	},
+	{
+		// Other model options go here
+	}
+);
